f2889e7b79ed14c3a21a22ea48125c1d80078c40
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-02-14 09:07:06 -0300

    Adiciona index.html

---
+++ node_3f8c501ea2768.tmp.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfólio Web</title>
+</head>
+<body>
+    <div><h1>João Pedro Costa Araujo</h1></div>
+    <div class="bordas"><img src="./30571932_1872340636174676_7961018893009420288_n.jpg" alt="Foto minha na neve" width="200 px" height="200 px" >
+    <style></style>
+    </div>
+    <div><h1><a href="#sobremim">Sobre mim</a></h1>
+    <p id="sobremim">Olá, meu nome é João Pedro, tenho 29 anos e sou de Sete Lagoas - MG, cidade pertinho da capital Belo Horizonte.
+        Sempre fui muito ligado à tecnologia. Comecei no mundo dos jogos muito cedo, jogando o amado Nintendo com minha irmã. </p>
+        <p>Sou quase formado em Turismo pela UFMG, mas atualmente estou me aventurando em Desenvolvimento Web na <a href="https://www.betrybe.com/" target="_blank">Trybe</a></p>
+    </div>
+       <div><p><h3>Como comecei recentemente o curso de Desenvolvimento Web, ainda estou aprendendo, mas ao final do curso espero ter habilidades com:</h3></p>
+        <ul>
+            <li class="javascript">Javascript</li>
+            <li class="sql">SQL</li>
+            <li class="react">React</li>
+            <li class="bootstrap">Bootstrap</li>
+            <li class="metoAgil">Metodologias ágeis</li>
+        </ul>
+    </div>
+</body>
+</html>
